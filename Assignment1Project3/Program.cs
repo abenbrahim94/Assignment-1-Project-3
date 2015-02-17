3fad348454374c82c04b5ff426c09d4ff8d517da
abenbrahim94 <abenbrahim94@gmail.com>
2015-02-17 10:58:11 -0500

    FinalCommit

---
+++ Assignment1Project3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variables//
+            string AreaCalc;
+            double radiusInput;
+            const double radius = 3.14d;
+            double TotalArea;
+
+            //Codes//
+            Console.WriteLine("\"Welcome to Program3\"");
+            Console.WriteLine();
+            Console.WriteLine("Insert Radius");
+            Console.WriteLine();
+            AreaCalc = Console.ReadLine();
+            radiusInput = Convert.ToDouble(AreaCalc);
+            TotalArea = radius * radiusInput * radiusInput;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("The Area of radius {0} is {1}", radiusInput, TotalArea);
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
